3127ed56233f3538803d59d9b5abd931ad62327a
Dheerendra Singh <dsingh110096@gmai.com>
2020-05-22 09:45:13 +0530

    Context api added

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ContactItem = ({ contact }) => {
+  const { id, name, email, phone, type } = contact;
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span
+          style={{ float: 'right' }}
+          className={'badge' + ' ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+          {/*basically here chatAt(0) will convert first letter to upparcase and show it erase all character of the word so to get all other charcter back we use slice type.slice(1) slice will leave of the first charater and all character will come back except first one(basically slice remove it from string) because we have already have at by using chatAt()*/}
+        </span>
+      </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i> {phone}
+          </li>
+        )}
+      </ul>
+      <p>
+        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button className='btn btn-danger btn-sm'>Delete</button>
+      </p>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+export default ContactItem;
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,72 @@
+import React, { useReducer } from 'react';
+//uuid generate random id becaues before working with api we will work with hardcoded data
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jane Doe',
+        phone: '876-768-876',
+        email: 'doe@gmail.com',
+        type: 'professional',
+      },
+      {
+        id: 2,
+        name: 'Zapata',
+        phone: '123-768-876',
+        email: 'zoe@gmail.com',
+        type: 'personal',
+      },
+      {
+        id: 3,
+        name: 'Kurt Weller',
+        phone: '890-768-876',
+        email: 'kurt@gmail.com',
+        type: 'professional',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+  //here state will allow us access any state in the state.
+  //dispatch will allow us disptach any object to the reducer that is contactReducer.
+
+  //Add Contact
+
+  //Delete Contact
+
+  //Set Current Contact
+
+  //Clear Current Contact
+
+  //Update Contact
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
