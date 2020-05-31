1b714b1d51da0427006f835d1b01d68f8c088bbb
Dheerendra Singh <dsingh110096@gmai.com>
2020-05-31 09:51:05 +0530

    Changes in About component

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const About = () => {
   return (
-    <div>
+    <Fragment>
       <h1>About This App</h1>
-      <h4 className='mb-1'>
-        This is a Fullstack Web App based on react,react
-        router,react-router-dom,nodejs,express,context.api etc. for keeping contacts
-      </h4>
+      <p>App to Keep Track Of Contacts Of A User</p>
       <p>
-        Bacically this app keeps tracks of contacts of users, they can create contacts, can
-        update,delete contacts etc
+        Based on :{' '}
+        <strong>React, ReactDom, Router, Context Api, Node.js,React-trasition-group</strong>{' '}
       </p>
-
-      <p className='p my-1' style={{ backgroundColor: 'orange', color: 'white' }}>
-        <strong>Version: </strong> 1.0.0
-      </p>
-    </div>
+      <p>Version 1.0.0</p>
+      <h3>Developer Info:</h3>
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src='https://avatars1.githubusercontent.com/u/15675693?v=4'
+            alt='Dheerendra Singh'
+            className='round-img'
+            style={{ width: '150px' }}
+          />
+          <h1>Dheerendra Singh</h1>
+          <p>Location: New Delhi, India</p>
+        </div>
+        <div>
+          <h3>Bio</h3>
+          <p style={{ marginBottom: '5px' }}>
+            Full Stack Developer, Specialized in JavaScript and It's Frameworks, CSS Frameworks.
+          </p>
+          <h3>Profiel Links</h3>
+          <ul>
+            <li>
+              <i className='fab fa-github' />{' '}
+              <a href='https://github.com/dsingh110096' target='_blank' rel='noopener noreferrer'>
+                dsingh110096
+              </a>
+            </li>
+            <li>
+              <i className='fab fa-linkedin-in' />{' '}
+              <a
+                href='https://www.linkedin.com/in/dheerendra-singh-7210b0128/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                dheerendra-singh-7210b0128
+              </a>
+            </li>
+            <li>
+              <i className='fab fa-facebook' />{' '}
+              <a
+                href='https://www.facebook.com/theGodOfCourage'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                theGodOfCourage
+              </a>
+            </li>
+            <li>
+              <i className='fab fa-instagram' />{' '}
+              <a
+                href='https://www.instagram.com/dheerendra3036/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                dheerendra3036
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
